a02f73d03631f2fdc17f396084be5f3370f3192f
Steven Davis <lifeofnations@msn.com>
2017-03-13 14:46:19 -0600

    added a working route to the zoomato api

---
+++ backend/routes/zoomatoRoutes.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var zomatoRoutes = express.Router();
+var request = require("request");
+var config = require("../../config");
+
+zomatoRoutes.route("/")
+    .get(function (req, res) {
+        if (req.query) {
+            var query = "?";
+            for (var key in req.query) {
+                query += key + "=" + req.query[key];
+            }
+        } else {
+            var query = "";
+        }
+        console.log(req.query);
+        var options = {
+            url: "https://developers.zomato.com/api/v2.1/search" + query,
+            headers: {
+               'user-key': config.key
+            }
+        };
+        request(options, function (err, response, body) {
+            if (err) return res.status(500).send(err);
+            res.send(body);
+        })
+    });
+
+module.exports = zomatoRoutes;
--- server.js
+++ server.js
@@ -1,36 +1,29 @@
-
-//install needed packages with npm  express body-parser mongoose morgan
-
-//require  express and other packages
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
-var config = require("./config");
+var config = require("./config");       //you will have to write your own config file to link here
 var mongoose = require("mongoose");
-var port = process.env.PORT || 8080;
-//declare a variable `app` = express();
-//declare a port variable
+var port = process.env.PORT || 8080;    //feel free to use any default port number you want
+
 
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+//we will eventually set up user auth middlewhere here with a secret of your choosing from your config file
 
-
-
-
-//set up body-parser and other middleware with app.use
 
 //set up routes for requests to link to
-
 app.use("/restaurant", require("./backend/routes/restaurantRoutes"));
+app.use("/zoomato", require("./backend/routes/zoomatoRoutes"));
 
-
-//set up and connect to database
-mongoose.connect("mongodb://localhost/team-taco", function (err) {
+//set up and connect to database the database is linked from your config file.
+//feel free to use whatever database name you want "mongodb://localhost/{{ your db name here }}"
+mongoose.connect(config.database, function (err) {
     if (err) throw err;
     console.log("database connected");
-})
+});
+
 //finish server with app.listen
 app.listen(port, function () {
     console.log("server active on port " + port);
-})
+});
