1a696f911c129d6889533b07e07a7080ede3ee7c
Steven Davis <lifeofnations@msn.com>
2017-03-21 11:16:08 -0600

    added stuff for the saving and updating of vacations

--- public/components/services/vacationService.js
+++ public/components/services/vacationService.js
@@ -2,15 +2,70 @@ angular.module("Main")
 
 .service("VacationService", ["$http", function ($http) {
 
+    var self = this;
+
     this.getVacations = function (user) {
         return $http.get("/secure/vacations?user=" + "user._id")
             .then(function (response) {
                 return response.data;
             },
             function (response) {
+                if (response.status === 404) {
+                    var newVacation = {
+                        name: "My Vacation",
+                        user: user
+                    };
+                    return self.createVacation(newVacation)
+                }
                 console.log(response);
                 alert("error");
             })
+    };
+
+    this.createVacation = function (vacation) {
+        return $http.post("/secure/vacation", vacation)
+            .then(function (resoponse) {
+                return resoponse.data
+            },
+            function (response) {
+                console.log(response);
+                alert("error")
+            })
+    };
+
+    this.updateVacation = function (vacation) {
+        return $http.post("/secure/vacation", vacation)
+            .then(function (response) {
+                return response.data
+            },
+            function (response) {
+                console.log(response);
+                alert("uh oh :(");
+            })
     }
 
-}]
+    this.saveRestaurant = function (restaurant) {
+        return $http.post("/secure/restaurant", restaurant)
+            .then(function (response) {
+                return response.data;
+            },
+            function (response) {
+                console.log(response);
+                alert("Oh boy..");
+            })
+    }
+
+    this.removeRestaurant = function (vacation, restaurant) {
+        return self.updateVacation(vacation)
+            .then(function (response) {
+                return response.data;
+            },
+            function (response) {
+                console.log(response);
+                alert("AAAhhhhhh!");
+            })
+    }
+
+
+
+}]);
