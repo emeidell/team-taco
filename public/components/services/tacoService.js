4cba1b5534df1e1c53646a23e1cbb981619fac60
Evan Meidell <ejmeidell@gmail.com>
2017-03-23 17:26:21 -0600

    Working Search button and Profile displays user display name

--- public/components/services/tacoService.js
+++ public/components/services/tacoService.js
@@ -7,15 +7,13 @@ angular.module("Main")
         if(query) {
             var queryString = "?";
             for (var key in query) {
-                queryString += key + query[key] + "&"
+                if (queryString.length > 1) queryString += "&";
+                queryString += key + "=" + query[key];
             }
-            queryString = queryString.split("");
-            queryString.pop();
-            queryString.join("");
+            console.log(queryString);
         } else {
             queryString = "";
         }
-        console.log(queryString);
         return $http.get('/zoomato' + queryString).then(function (response) {
             self.zoomatoResponse = response.data;
             return self.zoomatoResponse;
@@ -25,23 +23,19 @@ angular.module("Main")
     this.getZoomatoLocationId = function (city) {
         return $http.get('/zoomato/city?city=' + city)
             .then(function (response) {
-                console.log(response);
                 var query = {
                     'entity_id': response.data['location_suggestions'][0].entity_id,
                     'entity_type': response.data['location_suggestions'][0].entity_type
                 }
-                console.log(query);
                 return query;
             },
             function (response) {
-                console.log(response);
                 alert("HI, tooo bad!")
             })
             .then(function (response) {
                return self.getRequest(response)
             },
             function (response) {
-                // console.log(response);
                 alert("double Error");
             })
     }
